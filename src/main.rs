10a1706bcd633d36a623ced9d99a02639417f173
Ēriks Lapiņš <lapinseriks98@gmail.com>
2024-05-25 08:29:20 +0300

    dungeon crawler init

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+mod map;
+
+mod prelude {
+    pub use bracket_lib::prelude::*;
+    pub const SCREEN_WIDTH: i32 = 80;
+    pub const SCREEN_HEIGHT: i32 = 50;
+    pub use crate::map::*;
+}
+
+use prelude::*;
+
+
+const FPS_CAP: f32 = 30.0;
+struct State {
+    map: Map,
+}
+
+impl State {
+    fn new() -> Self {
+        State { map: Map::new() }
+    }
+}
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        self.map.render(ctx);
+    }
+}
+
+fn main() -> BError{
+    let context = BTermBuilder::simple80x50()
+        .with_title("Dungeon Crawler")
+        .with_fps_cap(FPS_CAP)
+        .build()?;
+    main_loop(context, State::new())
+}
