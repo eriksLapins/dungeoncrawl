fab5c8545a6599643d659abb2b3318646dbd98e2
Ēriks Lapiņš <lapinseriks98@gmail.com>
2024-05-30 19:51:29 +0300

    added game over screen

--- src/systems/combat.rs
+++ src/systems/combat.rs
@@ -2,6 +2,7 @@ use crate::prelude::*;
 
 #[system]
 #[read_component(WantsToAttack)]
+#[read_component(Player)]
 #[write_component(Health)]
 pub fn combat(
     ecs: &mut SubWorld,
@@ -14,12 +15,17 @@ pub fn combat(
         .collect();
     victims
         .iter().for_each(|(message, victim)| {
+            let is_player = ecs
+                .entry_ref(*victim)
+                .unwrap()
+                .get_component::<Player>()
+                .is_ok();
             if let Ok(health) = ecs
                 .entry_mut(*victim)
                 .unwrap()
                 .get_component_mut::<Health>() {
                     health.current -= 1;
-                    if health.current < 1 {
+                    if health.current < 1 && !is_player {
                         commands.remove(*victim);
                     }
                 }
